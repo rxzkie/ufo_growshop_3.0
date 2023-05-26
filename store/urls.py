255a8fada7ba3da65c235117c87607a4b1f1a0cb
asd <renzo_renzo_2001@hotmail.com>
2023-05-26 05:10:09 -0400

    static working with 2 views

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.index, name='semillas'),
+    path('semillas/', views.semillas, name='semillas'),
+    path('ofertas/', views.ofertas_view, name='ofertas'),
+    path('semillas/', views.semillas_view, name='semillas'),
+    path('parafernalia/', views.parafernalia_view, name='parafernalia'),
+    path('cultivo/', views.cultivo_view, name='cultivo'),
+    path('contacto/', views.contacto_view, name='contacto'),
 ]
--- store/views.py
+++ store/views.py
@@ -12,3 +12,24 @@ def index(request):
 def semillas(request):
     context = {}
     return render(request, 'store/semillas.html', context)
+
+
+def ofertas_view(request):
+    # Lógica de la vista para la página de Ofertas
+    return render(request, 'ofertas.html')
+
+def semillas_view(request):
+    # Lógica de la vista para la página de Semillas
+    return render(request, 'semillas.html')
+
+def parafernalia_view(request):
+    # Lógica de la vista para la página de Parafernalia
+    return render(request, 'parafernalia.html')
+
+def cultivo_view(request):
+    # Lógica de la vista para la página de Cultivo
+    return render(request, 'cultivo.html')
+
+def contacto_view(request):
+    # Lógica de la vista para la página de Contacto
+    return render(request, 'contacto.html')
