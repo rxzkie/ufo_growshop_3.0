d98a3b020bd1630e48663a72fa468927d9a7a76d
asd <renzo_renzo_2001@hotmail.com>
2023-05-30 14:24:03 -0400

    add model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Parafernalia(models.Model):
+    idparaf = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    tipo = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='items')
+
+    def __str__(self):
+        return self.nombre
