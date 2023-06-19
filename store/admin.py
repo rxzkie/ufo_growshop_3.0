949f4dcd71f2808877c369aa7dc015df30022c7d
asd <renzo_renzo_2001@hotmail.com>
2023-06-19 16:22:55 -0400

    owo

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
+from .models import CatParaf, Parafernalia
 
-from .models import Parafernalia
-
-# Register your models here.
-
+admin.site.register(CatParaf)
 admin.site.register(Parafernalia)
--- store/models.py
+++ store/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
 
+class CatParaf(models.Model):
+    id_cat_paraf = models.AutoField(primary_key=True, db_column='idCatParaf')
+    nombre_tipo = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.nombre_tipo
 
 class Parafernalia(models.Model):
     idparaf = models.CharField(primary_key=True, max_length=10)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
-    tipo = models.CharField(max_length=50)
-    imagen = models.ImageField(upload_to='items',null=True)
+    id_cat_paraf = models.ForeignKey(CatParaf, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='items', null=True)
 
     def __str__(self):
         return self.nombre
