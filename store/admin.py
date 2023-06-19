571bd7608afe0401705eae5d1ca897fc46a5930f
asd <renzo_renzo_2001@hotmail.com>
2023-06-19 19:42:33 -0400

    q

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CatParaf, Parafernalia
+from .models import CatParaf, Parafernalia,Proveedor
 
 admin.site.register(CatParaf)
 admin.site.register(Parafernalia)
+
+admin.site.register(Proveedor)
--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db import models
+
 
 class CatParaf(models.Model):
     id_cat_paraf = models.AutoField(primary_key=True, db_column='idCatParaf')
@@ -15,6 +15,20 @@ class Parafernalia(models.Model):
     precio = models.DecimalField(max_digits=8, decimal_places=2)
     id_cat_paraf = models.ForeignKey(CatParaf, on_delete=models.CASCADE)
     imagen = models.ImageField(upload_to='items', null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+
+class Proveedor(models.Model):
+    id_prov = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=20)
+    fecha_compra = models.DateField(blank=False, null=False)
+    telefono = models.CharField(max_length=45)
+    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
+    direccion = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
